9784f0a2dbf48cd65aa0235bb7b375ae119eb048
Desperationis <smarttdiego@gmail.com>
2021-10-15 20:38:49 -0700

    Greatly improve main.bash
    Three things were done that greatly improved this bot:
    1. The bot is has a much more detailed description of what its doing,
    allowing easier debugging.
    
    2. Everything is time-based according to bash instead of counter-based,
    which doesn't work for all devices.
    
    3. Special code was added for special events to better handle them

--- main.bash
+++ main.bash
@@ -2,30 +2,31 @@
 
 source adbAPI.bash
 
+export -f tap
+export -f swipe
+export -f longpress
+export -f screenshot
+export -f clickImageCache
+export -f clickFoundImage
+
+
 # Program start - assume device is unlocked and battlecats may or may not be
 # open.
 
+changeBrightness 0
+
 navigateToChapter() {
+	# Restart battle cats and navigate to main chapter menu.
+
 	restartBattleCats
-	scounter=0
-	while true
+
+	echo "Navigating to the main chapter menu."
+	until clickFoundImage skip.png 15 && { clickFoundImage play.png 15 && clickFoundImage chapter.png 15; }
 	do
-		screenshot
-		if clickImageCache skip.png
-		then
-			break
-		fi
-
-		if [[ scounter -ge 15 ]]
-		then
-			restartBattleCats
-			scounter=0
-		fi
-
-		((scounter++))
+		echo "There was an error navigating main menu. Restarting..."
+		restartBattleCats
 	done
-	clickFoundImage play.png
-	clickFoundImage chapter.png
+	echo "Arrived at main chapter menu."
 }
 
 counter=0
@@ -33,123 +34,117 @@ navigateToChapter
 
 while true
 do
+	echo " "
 
-	# Very important. This section clicks any daily banners / awards until
-	# catfood has been pressed. The 2s wait is to wait for banners
+	# Check if we are in main menu or daily special event menu
+	echo "Checking screenshot to confirm we are at the menu."
 	sleep 2
-	ccounter=0
-	while true
-	do
-		screenshot
-		clickImageCache bigOk.png
-		clickImageCache smallOk.png
-		clickImageCache ok.png
-
-		clickImageCache eventX.png 
-		echo "Searching xx"
-
-		if [[ $ccounter -gt 1 ]]
-		then
-			if clickImageCache catfood.png .95
-			then
-				break
-			fi
-		fi
-
-		# Restart if for whatever reason we get stuck
-		if [[ $ccounter -gt 5 ]] 
-		then
-			restartBattleCats
-			quit=true
-			break
-		fi
-
-		((ccounter++))
-	done
-	echo "Exit"
-
-	ccounter=0
-	quit=false
-	while true
-	do
-		screenshot
-		if clickImageCache watch.png 
-		then
-			break
-		fi
-
-		# Restart if for whatever reason we get stuck
-		if [[ $ccounter -gt 5 ]] 
-		then
-			restartBattleCats
-			quit=true
-			break
-		fi
-	done
-
-	if [[ $quit == true ]]
+	screenshot
+	if imageFound start.png
 	then
+		echo "We are in the chapter menu. Trying to click catfood."
+		if ! clickFoundImage catfood.png 10 .95
+		then
+			echo "Not able to find catfood on chapter menu. Restarting loop."
+			continue
+		else
+			echo "Catfood was clicked."
+		fi
+	else
+		echo "We are not in the chapter menu. Trying to close daily events."
+
+		SECONDS=0
+		until (( SECONDS >= 60 )) || imageFound start.png
+		do
+			screenshot
+			clickImageCache bigOk.png
+			clickImageCache smallOk.png
+			clickImageCache ok.png
+			clickImageCache eventX.png 
+			clickImageCache back.png 
+		done
+
+		if (( SECONDS >= 60 ))
+		then
+			echo "Not able to close events. Restarting Battle Cats and loop."
+			navigateToChapter
+			continue
+		else
+			echo "Able to close all events. Restarting loop."
+			continue
+		fi
+	fi
+
+
+
+	# Click ad offer and repeat loop if unable to
+	echo "Trying to click ad offer."
+	SECONDS=0
+	if clickFoundImage watch.png 15
+	then
+		echo "Offer was able to be clicked."
+	else
+		echo "Offer was unable to be clicked. Restarting Battle Cats and loop."
+		navigateToChapter
 		continue
 	fi
 
-	timer=0
+
+
+	# Auto close the ad
+	echo "Watching ad and trying to close it. This might take awhile."
+	SECONDS=0
 	while true
 	do
-		echo $timer
 		screenshot
 
 		if clickImageCache ok.png || clickImageCache systemok.png
 		then
+			echo "Ad has ended."
 			break
 		fi
 
-		if [[ $timer -gt 4 ]]
+		if (( SECONDS >= 5 ))
 		then
 			if python3 isBlack.py
 			then
-				counter=0
+				echo "Ad crashed. Restarting battlecats and loop."
 				navigateToChapter
 				break
 			fi
 		fi
 
-		if [[ $timer -gt 15 ]]
+		if (( SECONDS >= 15 ))
 		then
-
+			# Scan ads
 			for file in imgdec/ads/*
 			do
 				if clickImageCache "${file#imgdec/}" .97
 				then
-					touch "$file"
+					touch "$file" # Used to keep track of rarely used ad images
 				fi
 			done
 		fi
 
-		if [[ $timer -gt 45 ]]
+		if (( SECONDS >= 45 ))
 		then
-			mkdir stuck
+			echo "Can't close this ad, moving screenshot to stuck/ and restarting."
+
+			if ! [[ -d stuck ]]
+			then
+				mkdir stuck
+			fi	
+
 			mv screen.png stuck/$(date +%s).png
-			counter=0
 			navigateToChapter
 			break
 		fi
 
 		if ! battleCatsRunning
 		then
-			counter=0
+			echo "Foreign app was opened. Restarting battlecats and loop."
 			navigateToChapter
 			break
 		fi
-
-		# Timer is solely based on the fact that "screenshot" func takes about 1 sec
-		((timer++))
 	done
-
-	(( counter++ ))
-
-	if [[ $counter -ge 5 ]]
-	then
-		navigateToChapter
-		counter=0
-	fi
 done
